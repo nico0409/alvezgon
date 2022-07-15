55777999f681c3cab94ff1c374c47e567bd4deae
nicolas gonzalez <nico04093@gmail.com>
2022-07-15 01:32:03 -0300

    correcion

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,5 @@
-//export const BASE_PATH = "http://localhost:1337";
 //export const BASE_PATH = "http://localhost:8080";
 export const BASE_PATH = "https://alvezgon-node.herokuapp.com";
 export const TOKEN = "token";
 export const CART = "cart";
-export const STRIPE_TOKEN =
-  "pk_test_51LIj0iHQB3mtIJdts4cGeZvxNl8PdKbc6wiq5LI533zPAl4Vfx8Yji6lETn0VJJmLqk8kRooLXkRZCcoiiBhPXTm00Uy7IAE2i";
+export const STRIPE_TOKEN = TOKENSTRAPI;
